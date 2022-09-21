016c046674ad2940783c5ca2413ab71fe708597d
Arturo Almagro <almagromendezarturo@gmail.com>
2022-09-21 15:16:25 +0200

    Now,the customers are saved with the first character in capital

--- Customer_Controller/buscador.py
+++ Customer_Controller/buscador.py
@@ -1,19 +1,20 @@
-customer_list = [] #On this list/Array we will save the customers
+customer_list = []  # On this list/Array we will save the customers
 
 
 def saveCustomer():
-    new_customer = input("What's the name of the customer? ") #Ask for the name of the customer
-    customer_list.append(new_customer) #Add the customer to the list
+    # Ask for the name of the customer
+    new_customer = input("What's the name of the customer? ").capitalize()
+    customer_list.append(new_customer)  # Add the customer to the list
     print("The customer " + new_customer + " is saved at the position " +
-          str(customer_list.index(new_customer))) #Show message if the save was successful
-    answer = input("Do you want to save more customers? (y/n) ") 
+          str(customer_list.index(new_customer)))  # Show message if the save was successful
+    answer = input("Do you want to save more customers? (y/n) ")
     if answer == "y":
         saveCustomer()
 
 
-
-def show_customers(customer_list): #Function that shows the list of customers in order they were created
-    for i in range(len(customer_list)): 
+# Function that shows the list of customers in order they were created
+def show_customers(customer_list):
+    for i in range(len(customer_list)):
         print("The customer number " + str(i) + " is : " + customer_list[i])
 
 
