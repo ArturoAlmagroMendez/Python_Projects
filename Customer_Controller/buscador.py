03baed1c37ceae1ec1637b0f32fe4b1f51de34f2
Arturo Almagro <almagromendezarturo@gmail.com>
2022-09-21 14:37:50 +0200

    v.02 experimental,almost finished

--- Customer_Controller/buscador.py
+++ Customer_Controller/buscador.py
@@ -1,29 +1,29 @@
 client_list = []
 def saveCustomer():
 
-   
-    new_client = input("What's the name of the client? ")
+    new_client = input("What's the name of the customer? ")
     client_list.append(new_client)
     print("The client " + new_client + " is saved at the position " + str(client_list.index(new_client)))
+    answer = input("Do you want to save more customers? (y/n) ")
+    if answer == "y":
+        saveCustomer()
+
+    answer2 = input("Do you want to see all the customers? (y/n) ")
+    if answer2 == "y":
+        show_customers(client_list)
+        print
+    else:
+        print("Okay,see you later =)")
+    
   
-        
-
 def show_customers(client_list):
-    for i in range(client_list):
-        print(client_list[i])
-
+    for i in range(len(client_list)):
+        print("Customer number " + str(i) + " : " + client_list[i])
 
 
 def run():
     saveCustomer()
-    answer = input("Do you want to save more customers? (y/n) ")
-    while answer == "y":
-        saveCustomer()
-    answer2 = input("Do you want to see all the customers? (y/n) ")
-    if answer2 == "y":
-        show_customers(client_list)
-    else:
-        print("Okay,see you later =)")
+    
 
 
 if __name__ == '__main__':
