984b4fa5c1b9db3f093044b8140def5baa892b41
Arturo Almagro <almagromendezarturo@gmail.com>
2022-09-24 16:36:40 +0200

    merge(Protein_Calculator): merging develop and main

--- Protein_Calculator/controller.py
+++ Protein_Calculator/controller.py
@@ -1,7 +1,7 @@
 aliments = ("Chicken", "Tuna", "Integral rice", "Pasta", "Quinoa",
             "Oatmeal", "Eggs", "Cooked ham", "Serrano ham", "Omelette", "Bacon")
-proteins = (27, 29, 2.6, 5, 16.5, 14, 14.18, 21, 31, 11, 37)
-
+proteins = (27, 29, 2.-6, 5, 16.5, 14, 14.18, 21, 31, 11, 37)
+ 
 
 
 def calc_proteins(quantity, protein):
@@ -27,14 +27,15 @@ def print_main_menu():
     option = int(input(
         "Choose one and lets calculate the proteins!! (input option 1 - " + str(len(aliments)) + ") : "))
 
-    if option in range(1, len(aliments)):
-        quantity = (int(input("How many grams of " + aliments[int(option) - 1] + " ??? ")))
-        result = calc_proteins(quantity, int(proteins[int(option) - 1]))
-        result = round(result,2)
+    if option in range(1, len(aliments) + 1): #we have to sum 1 because the option 1 is the position 0 of the tuple
+        quantity = (int(input("How many grams of " + aliments[int(option) - 1] + " ??? "))) 
+        result = calc_proteins(quantity, int(proteins[int(option) - 1])) #we have to substract 1 because the option 1 is the position 0 of the tuple
+         #result = round(result,2)
         print(str(quantity) + " g of " + aliments[int(option) - 1] + " have " + str(result) + " g of proteins")
 
 
 def run():
+    #print(str(proteins[2]) + " " + str(proteins[4]))
     print_main_menu()
 
 
